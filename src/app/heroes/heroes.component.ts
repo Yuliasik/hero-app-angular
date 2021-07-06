adfd94db3ee0aebcedbedef8eab50aa2684c9879
yulia_04 <yulichka.dudij@gmail.com>
2021-07-06 03:39:40 +0300

    add styles

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -31,6 +31,21 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+  
   // getHero(id: number): Observable<Hero> {
   //   // For now, assume that a hero with the specified `id` always exists.
   //   // Error handling will be added in the next step of the tutorial.
